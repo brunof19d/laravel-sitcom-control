5c2fe00e53fad9e403d8671919b674332400e822
brunof19d <brunof19d@gmail.com>
2020-08-10 01:49:09 +0100

    add form request validation

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Serie;
 use Illuminate\Http\Request;
+use App\Http\Requests\SeriesFormRequest;
 
 class SeriesController extends Controller
 {
@@ -18,7 +19,7 @@ class SeriesController extends Controller
         return view ('series.create');
     }
 
-    public function store(Request $request) 
+    public function store(SeriesFormRequest $request) 
     {
         $serie = Serie::create($request->all());
         $request->session()
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -6,6 +6,16 @@ Add sitcom
  
 @section('content')
 
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 <form method="POST">
     @csrf
     <div class="form-group">
