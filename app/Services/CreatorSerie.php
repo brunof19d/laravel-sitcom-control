0a71a1cdb8514292c8bf551f8a295d756eaeeffa
brunof19d <brunof19d@gmail.com>
2020-08-13 14:53:33 +0100

    improving the class of creation

--- app/Services/CreatorSerie.php
+++ app/Services/CreatorSerie.php
@@ -3,21 +3,34 @@
 namespace App\Services;
 
 use App\Serie;
+use Illuminate\Support\Facades\DB;
 
 class CreatorSerie
 {
-    public function createSerie(string $nameSerie, int $numberSeason, int $numberEpisode) : Serie
+    public function createSerie(string $nameSerie, int $numberSeasons, int $numberEpisode): Serie
     {
+        $serie = null;
+        DB::beginTransaction();
         $serie = Serie::create(['name' => $nameSerie]);
-        $numberSeasons = $numberSeason;
+        $this->makeSeasons($numberSeasons, $numberEpisode, $serie);
+        DB::commit();
 
+        return $serie;
+    }
+
+    public function makeSeasons(int $numberSeasons, int $numberEpisode, Serie $serie)
+    {
         for ($i = 0; $i <= $numberSeasons; $i++) {
             $season = $serie->seasons()->create(['number' => $i]);
 
-            for ($j = 1; $j <= $numberEpisode; $j++) {
-                $season->episodes()->create(['number' => $j]);
-            }
+            $this->makeEpisodes($numberEpisode, $season);
         }
-        return $serie;
+    }
+
+    public function makeEpisodes(int $numberEpisode, \Illuminate\Database\Eloquent\Model $season): void
+    {
+        for ($j = 1; $j <= $numberEpisode; $j++) {
+            $season->episodes()->create(['number' => $j]);
+        }
     }
 }
